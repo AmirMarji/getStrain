7854c54b8e5f82d7f7a2bc44d26dbc7e47532d9b
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2023-06-15 16:09:32 -0400

    imported to pycharm and made files more 'pythonic'.

--- getStrain.py
+++ getStrain.py
@@ -2,26 +2,28 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-def main():
-    getStrain()
 
-def getStrain():
+def main():
+    get_strain()
+
+
+def get_strain():
     r = requests.get('https://www.aznaturalselections.com/education/strains/list/')
     soup = BeautifulSoup(r.content, 'html.parser')
     # print(soup.prettify())
-    
+
     for strain in soup.find_all('div', class_='wp-show-posts-inner'):
         name = strain.find('h3', class_='wp-show-posts-entry-title').text.strip()
-        strainType = strain.find('div', class_='wp-show-posts-entry-summary').text.strip()
-        print(name, strainType)
+        strain_type = strain.find('div', class_='wp-show-posts-entry-summary').text.strip()
+        print(name, strain_type)
         print()
-        strainData = { 'name': name, 'strainType': strainType }
-        
-        #export all of the data to a json file
+        strain_data = {'name': name, 'strainType': strain_type}
+
+        # export all the data to a json file
         with open('strainData.json', 'a') as outfile:
-            json.dump(strainData, outfile)
+            json.dump(strain_data, outfile)
             outfile.write('\n')
-            
+
 
 if __name__ == '__main__':
     main()
